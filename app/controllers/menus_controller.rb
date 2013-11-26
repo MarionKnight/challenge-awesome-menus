a4dd5d06e53290800d8fa2d07258e8efb311bd4a
MarionKnight <knight499@gmail.com>
2013-11-25 18:53:25 -0800

    debug stash on template error

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -6,6 +6,8 @@ class MenusController < ApplicationController
 
   def show
     @menu = Menu.find params[:id]
+    @menu_items = MenuItem.where(menu_id: params[:id])
+    @menu_item = MenuItem.new
   end
 
   def create
